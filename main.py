6705dd24ce824c76e9c6270a1ef40bcba006fc1c
Elia Mazzon <ailenozzam@gmail.com>
2022-02-23 17:07:41 -0600

    added search function for albums and song

--- main.py
+++ main.py
@@ -1,29 +1,64 @@
 from pytube import YouTube
 from pytube import Playlist
+from ytmusicapi import YTMusic
 
-while(True):
-    link = input("\nPaste ur link here (or 'q' to exit): ")
-    if link.lower().strip() == "q":
-        break;
+ytmusic = YTMusic()
+
+def mdown(id,url):
+    if id == None:
+        link = url
     else:
-        pass;
+        link = "https://music.youtube.com/"+id
+    print(link)
     try:
         yt = YouTube(link);
+        print(f"Donwloading {yt.title}")
+        yt.streams.filter(only_audio=True,abr="160kbps").first().download(output_path="/home/aile_/Documents/CODING/python/mdwn/music/",filename=f"{yt.title}")
+        print("\nDONE\n")
     except:
-        if input("\n is it an album or a playlist? (y/n)") == "y":
+        try:
             p= Playlist(link)
-            chuck = input(f"\n Is this the album/playlist u want to download (y/n): \n {p.title}")
-            if chuck.lower().strip()=="y":
-                pass;
-            else:
-                print("ups!")
-                continue;
             for song in p.videos:
                 print(f"Downloading {song.title}")
-                song.streams.filter(only_audio=True,abr="160kbps").first().download(output_path=f"/home/aile_/Documents/CODING/python/mdwn/music/{p.title}",filename=f"{song.title}")
+                song.streams.filter(only_audio=True,abr="160kbps").first().download(output_path=f"/home/aile_/Music/{p.title}",filename=f"{song.title}")
             print("\nDONE\n")
+        except:
+            print("\nAn error occurred, try again...\n")
 
-        continue;
-    print(f"Donwloading {yt.title}")
-    yt.streams.filter(only_audio=True,abr="160kbps").first().download(output_path="/home/aile_/Documents/CODING/python/mdwn/music/",filename=f"{yt.title}")
-    print("\nDONE\n")
+
+
+
+while True:
+    mode = input("Chose MODE album(a) song(s) url(u): ")
+    if mode.lower().strip() == "a":
+        mode = "albums"
+        key = "browseId"
+    elif mode.lower().strip() == "s":
+        mode = "songs"
+        key = "videoId"
+
+    elif mode.lower().strip() == "u":
+        url = input("\npaste URL here: ")
+        id = None
+        mdown(id,url)
+        continue
+    else:
+        print("sorry try again")
+        continue
+
+    results = ytmusic.search(input("\nType the song/album name here: "),filter=f"{mode}")
+    print("\nHere's what we found: \n")
+    n=0
+    for song in results:
+        title = song['title']
+        art = song['artists']
+        artist = (art[0])['name']
+        print(f" {n}: {title} by {artist}")
+        n+=1
+    sn=input(f"\nType in the song/album number (0 to {n-1}): \n")
+    if mode == "albums":
+        alb_dt = ytmusic.get_album(browseId=f"{(results[int(sn)])[key]}")
+        id = "playlist?list=" + alb_dt['audioPlaylistId']
+    else:
+        id = "watch?v=" + (results[int(sn)])[key]
+    mdown(id)
